f18406613d271f568d0c53674142b432cc6b2490
cthnguyen <cthnguyen1995@gmail.com>
2019-06-19 17:08:15 +0200

    rename file spee_dating.py to flask.py

---
+++ flask.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template, session, request, redirect, flash, url_for
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
