08b444831c52ca27775a85d1bb7f077d05b5fd88
Lim Pascal <pascal.lim@telecom-paristech.fr>
2019-06-20 17:40:44 +0200

    update sankey

--- js/chart_sankey.js
+++ js/chart_sankey.js
@@ -32,7 +32,7 @@ d3.json("data/data_sankey.json", function(error, graph) {
   sankey
       .nodes(graph.nodes)
       .links(graph.links)
-      .layout(1);
+      .layout(0);
 
   // add in the links
   var link = svg.append("g")
@@ -48,14 +48,14 @@ d3.json("data/data_sankey.json", function(error, graph) {
   // add in the nodes
   var node = svg.append("g")
     .selectAll(".node")
-    .data(graph.nodes)
+	.data(graph.nodes.sort(function (a,b) {return d3.ascending(a.value, b.value); }))
     .enter().append("g")
       .attr("class", "node")
-      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
-      .call(d3.drag()
-        .subject(function(d) { return d; })
-        .on("start", function() { this.parentNode.appendChild(this); })
-        .on("drag", dragmove));
+      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+	  // .call(d3.drag()
+        // .subject(function(d) { return d; })
+        // .on("start", function() { this.parentNode.appendChild(this); })
+        // .on("drag", dragmove));
 
   // add the rectangles for the nodes
   node
@@ -76,7 +76,7 @@ d3.json("data/data_sankey.json", function(error, graph) {
 		})					
 	   .on("mouseout", function(d) {		
 			div.transition()		
-				.duration(500)		
+				.duration(500)
 				.style("opacity", 0)
 		});
 
@@ -94,16 +94,16 @@ d3.json("data/data_sankey.json", function(error, graph) {
         .attr("text-anchor", "start");
 
   // the function for moving the nodes
-  function dragmove(d) {
-    d3.select(this)
-      .attr("transform", 
-            "translate(" 
-               + d.x + "," 
-               + (d.y = Math.max( 
-                  0, Math.min(height - d.dy, d3.event.y)) 
-                 ) + ")"); 
-    sankey.relayout();
-    link.attr("d", sankey.link() ); 
-  }
+  // function dragmove(d) {
+    // d3.select(this)
+      // .attr("transform", 
+            // "translate(" 
+               // + d.x + "," 
+               // + (d.y = Math.max( 
+                  // 0, Math.min(height - d.dy, d3.event.y)) 
+                 // ) + ")"); 
+    // sankey.relayout();
+    // link.attr("d", sankey.link() ); 
+  // }
 
 });
