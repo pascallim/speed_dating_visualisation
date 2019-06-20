5a3291c893e7951ecd6b3c52432f15aaf7f917c9
Lim Pascal <pascal.lim@telecom-paristech.fr>
2019-06-20 11:40:41 +0200

    update sankey

--- js/chart_sankey.js
+++ js/chart_sankey.js
@@ -17,8 +17,13 @@ var color = d3.scaleOrdinal(d3.schemeCategory20);
 // Set the sankey diagram properties
 var sankey = d3.sankey()
     .nodeWidth(25)
-    .nodePadding(0)
+    .nodePadding(5)
     .size([width, height]);
+	
+// Define the div for the tooltip
+var div = d3.select("body").append("div")	
+    .attr("class", "tooltip")				
+    .style("opacity", 0);
 
 // load the data
 d3.json("data/data_sankey.json", function(error, graph) {
@@ -54,14 +59,26 @@ d3.json("data/data_sankey.json", function(error, graph) {
 
   // add the rectangles for the nodes
   node
-    .append("rect")
+    .append("rect")	
       .attr("height", function(d) { return d.dy; })
       .attr("width", sankey.nodeWidth())
+	  .attr("rx", 8)
+	  .attr("ry", 8)
       .style("fill", function(d) { return d.color = color(d.name.replace(/ .*/, "")); })
-      .style("stroke", function(d) { return d3.rgb(d.color).darker(2); })
-    // Add hover text
-    .append("title")
-      .text(function(d) { return d.name + "\n" + "There is " + d.value + " stuff in this node"; });
+      // .style("stroke", function(d) { return d3.rgb(d.color).darker(2); })
+	  .on("mouseover", function(d) {		
+            div.transition()		
+                .duration(200)		
+                .style("opacity", .9);		
+            div	.html(d.name)	// define text of the tooltip
+                .style("left", (d3.event.pageX) + "px")		
+                .style("top", (d3.event.pageY - 28) + "px")
+		})					
+	   .on("mouseout", function(d) {		
+			div.transition()		
+				.duration(500)		
+				.style("opacity", 0)
+		});
 
   // add in the title for the nodes
     node
@@ -78,15 +95,15 @@ d3.json("data/data_sankey.json", function(error, graph) {
 
   // the function for moving the nodes
   function dragmove(d) {
-    // d3.select(this)
-      // .attr("transform", 
-            // "translate(" 
-               // + d.x + "," 
-               // + (d.y = Math.max( 
-                  // 0, Math.min(height - d.dy, d3.event.y)) 
-                 // ) + ")"); 
+    d3.select(this)
+      .attr("transform", 
+            "translate(" 
+               + d.x + "," 
+               + (d.y = Math.max( 
+                  0, Math.min(height - d.dy, d3.event.y)) 
+                 ) + ")"); 
     sankey.relayout();
-    // link.attr("d", sankey.link() ); 
+    link.attr("d", sankey.link() ); 
   }
 
 });
