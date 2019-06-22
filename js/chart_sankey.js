38d564fae53b180f4bd7f37299eace27bf5928a1
Lim Pascal <pascal.lim@telecom-paristech.fr>
2019-06-22 02:55:38 +0200

    update

--- js/chart_sankey.js
+++ js/chart_sankey.js
@@ -12,15 +12,15 @@ var svg = d3.select("#my_dataviz").append("svg")
           "translate(" + margin.left + "," + margin.top + ")");
 
 // Color scale used
-// var color = d3.scaleOrdinal(d3.schemeCategory20); 
 var prevCol = "";
 var prevColR = "";
-var Color1_H = "#7aa5fa";
-
+var click = 0;
+var color_click = "rgb(95, 101, 112)";
+var node_click = 0;
 
 // Text Scale
 var textScale = d3.scaleLinear()
-    .range([70, 130]);
+    .range([70, 115]);
 
 // Set the sankey diagram properties
 var sankey = d3.sankey()
@@ -39,7 +39,7 @@ var div_node = d3.select("body").append("div")
 
 
 // load the data
-d3.json("data/data_sankey.json", function(error, graph) {
+d3.json("data/test.json", function(error, graph) {
   
   // Constructs a new Sankey generator with the default settings.
   sankey
@@ -57,19 +57,28 @@ d3.json("data/data_sankey.json", function(error, graph) {
       .attr("d", sankey.link() )
       .style("stroke-width", function(d) { return Math.max(1, d.dy); })
       .sort(function(a, b) { return b.dy - a.dy; })
-	  .on("mouseover", function(d) {		
-		div_link.transition()		
-			.duration(200)		
-			.style("opacity", .9);		
-		div_link.html(Math.round(d.value/d.source.value*100) + "% of the men in the <strong>"  +  d.source.name + "</strong> Category matched with " + Math.round(d.value/d.target.value*100) + "% of the women in the <strong>"  +  d.target.name + "</strong> Category.")	// define text of the tooltip
-			.style("left", (d3.event.pageX) + "px")		
-			.style("top", (d3.event.pageY - 28) + "px")
-		})
-	   .on("mouseout", function(d) {		
+	  .on("mouseover", function(d) {
+		if (d.source.node==node_click && click==1) {
+			div_link.transition()		
+				.duration(200)		
+				.style("opacity", .9);		
+			div_link.html(Math.round(d.value/d.source.value*100) + "% of the men in the <strong>"  +  d.source.name + "</strong> Category matched with " + Math.round(d.value/d.target.value*100) + "% of the women in the <strong>"  +  d.target.name + "</strong> Category.")	// define text of the tooltip
+				.style("left", (d3.event.pageX) + "px")		
+				.style("top", (d3.event.pageY - 28) + "px")
+		} else if (click==0) {
+			div_link.transition()		
+				.duration(200)		
+				.style("opacity", .9);		
+			div_link.html(Math.round(d.value/d.source.value*100) + "% of the men in the <strong>"  +  d.source.name + "</strong> Category matched with " + Math.round(d.value/d.target.value*100) + "% of the women in the <strong>"  +  d.target.name + "</strong> Category.")	// define text of the tooltip
+				.style("left", (d3.event.pageX) + "px")		
+				.style("top", (d3.event.pageY - 28) + "px")
+		}
+		// d3.selectAll(".link").filter(function(d) { return d.source.node != graph.nodes[i].node && d.target.node != graph.nodes[i].node })
+	  })
+	  .on("mouseout", function(d) {		
 		div_link.transition()		
 			.duration(500)
 			.style("opacity", 0)
-		
 		});
 
   // add in the nodes
@@ -79,10 +88,6 @@ d3.json("data/data_sankey.json", function(error, graph) {
     .enter().append("g")
       .attr("class", "node")
       .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-	  // .call(d3.drag()
-        // .subject(function(d) { return d; })
-        // .on("start", function() { this.parentNode.appendChild(this); })
-        // .on("drag", dragmove));
 
   // add the rectangles for the nodes
   node
@@ -91,22 +96,61 @@ d3.json("data/data_sankey.json", function(error, graph) {
       .attr("width", sankey.nodeWidth())
 	  .attr("rx", 4)
 	  .attr("ry", 4)
-      .style("fill", checkPositionAndColorize); // Color
-	  // .on("mouseover", function(d) {		
-            // div_node.transition()		
-                // .duration(200)		
-                // .style("opacity", .9);		
-            // div_node.html(d.name)	// define text of the tooltip
-                // .style("left", (d3.event.pageX) + "px")		
-                // .style("top", (d3.event.pageY - 28) + "px")
-		// })		
-	   // .on("mouseover",nodeMouseover)
-	   // .on("mouseout", function(d) {		
-			// div_node.transition()		
-				// .duration(500)
-				// .style("opacity", 0)
-			// d3.select(this).style("fill", "#334594");
-		// });
+      .style("fill", checkPositionAndColorize) // Color
+	  .on("click", function(d, i) {
+			if (click == 0) {
+				// Reset Colors
+				prevCol = ""
+				prevColR = ""
+				d3.selectAll("rect").style("fill", checkPositionAndColorize)
+				// Change color
+				d3.select(this).style("fill", color_click)
+				// Show only this node links
+				d3.selectAll(".link").style("stroke", "#000").style("stroke-opacity", 0.3)
+				d3.selectAll(".link").filter(function(d) { return d.source.node != graph.nodes[i].node && d.target.node != graph.nodes[i].node }).style("stroke-opacity", 0.0).style("stroke", "#000")
+				// Change state value
+				click = 1
+			} else if (click == 1) {
+				if (d3.select(this).style("fill") == color_click){
+					// Reset Colors
+					prevCol = ""
+					prevColR = ""
+					d3.selectAll("rect").style("fill", checkPositionAndColorize)
+					// Show all nodes
+					d3.selectAll(".link").style("stroke-opacity", 0.1).style("stroke", "#000");
+					// Change state value
+					click = 0
+					// alert(d3.select(this.parentNode).name)
+					// console.log(node_click)
+				} else {
+					// Reset Colors
+					prevCol = ""
+					prevColR = ""
+					d3.selectAll("rect").style("fill", checkPositionAndColorize)
+					// Set color
+					d3.select(this).style("fill", color_click)
+					// Show all nodes
+					d3.selectAll(".link").style("stroke-opacity", 0.1).style("stroke", "#000");
+					// Show only this node links
+					d3.selectAll(".link").style("stroke", "#000").style("stroke-opacity", 0.3)
+					d3.selectAll(".link").filter(function(d) { return d.source.node != graph.nodes[i].node && d.target.node != graph.nodes[i].node }).style("stroke-opacity", 0.0).style("stroke", "#000")
+					// node_click = alert(this.parentElement.parentElement.className)
+				}
+			}
+		})		
+	   .on("mouseover", function(d) {
+			div_node.transition()		
+                .duration(200)		
+                .style("opacity", .9)	
+            div_node.html("There are <strong>" + d.value +  "</strong> persons that have matched in the category '<strong>" + d.name + "</strong>'")	// define text of the tooltip
+                .style("left", (d3.event.pageX) + "px")		
+                .style("top", (d3.event.pageY - 28) + "px")
+	    })
+	   .on("mouseout", function(d) {		
+			div_node.transition()		
+				.duration(500)
+				.style("opacity", 0)
+		});
 
   // add in the title for the nodes
 	textScale.domain(d3.extent(graph.nodes, function(d){return d.value;}));
@@ -132,7 +176,7 @@ d3.json("data/data_sankey.json", function(error, graph) {
         .attr("x", 6 + sankey.nodeWidth())
         .attr("text-anchor", "start");
 
-		
+	// Color the rectangles
 	function checkPositionAndColorize(obj){
 		if(obj.x == 0 && prevCol == "") {
 			prevCol = "#7aa5fa";
@@ -147,7 +191,7 @@ d3.json("data/data_sankey.json", function(error, graph) {
 			return "#7aa5fa";
 		  }
 		  else if(obj.x !== 0 && prevColR == "") {
-			prevColR = "#fa7aaf";
+			prevColR = "#fa7aaf"; 
 			return "#fa7aaf";
 		  }
 		  else if(obj.x !== 0 && prevColR == "#fa7aaf"){
@@ -159,119 +203,5 @@ d3.json("data/data_sankey.json", function(error, graph) {
 			return "#fa7aaf";
 		  }
 	}
-	
-	function nodeMouseover(node, i){
-		// console.log(node.name);
-		var col = d3.select(this).style("fill");
-		console.log(col)
-		d3.selectAll(".link").style("stroke", col).style("stroke-opacity", 0.2);
-		d3.selectAll(".link").filter(function(d) { return d.source.name != graph.nodes[i].name && d.target.name != graph.nodes[i].name }).style("stroke-opacity", 0.0).style("stroke", "#000");
-		d3.select(this).style("fill", "#334594");
-		// d3.select(this.parentNode).moveToFront();
-		// this.__data__.targetLinks.sort(function(a,b){
-		  // return parseFloat(b.value) - parseFloat(a.value);
-		// });
-
-		// this.__data__.sourceLinks.sort(function(a,b){
-		  // return parseFloat(b.value) - parseFloat(a.value);
-		// });
-
-
-		// var thiz = this.__data__;
-		// var targetData = "";
-		// var testArray = [];
-		// for (var i = 0; i < this.__data__.targetLinks.length; i++){
-		  // var objToPush = {};
-		  // objToPush[this.__data__.targetLinks[i].source.name] = this.__data__.targetLinks[i].value;
-		  // testArray.push(objToPush);
-		// }
-		// var newTarData = combineKeyData(testArray.slice(0, 10));
-		// for (var prprt in newTarData){
-		  // targetData += "<tr><td class='cou-name'>" + prprt + " : </td><td class='cou-val'>" + formatNumber(newTarData[prprt]) + "</td></tr>";
-		// }
-		  
-		// var sourceData = "";
-		// var newArray = [];
-		// for (var z = 0; z < this.__data__.sourceLinks.length; z++){
-		  // var srcObjToPush = {};
-		  // srcObjToPush[this.__data__.sourceLinks[z].target.name] = this.__data__.sourceLinks[z].value;
-		  // newArray.push(srcObjToPush);
-		// }
-		// var newSrcData = combineKeyData(newArray.slice(0, 10));
-		// for (var proprt in newSrcData){
-		  // sourceData += "<tr><td class='cou-name'>" + proprt + " : </td><td class='cou-val'>" + formatNumber(newSrcData[proprt]) + "</td></tr>";
-		// }
-
-		// var tipText = "";
-		// if (this.__data__.targetLinks.length>0){
-		  // tipText = targetData;
-		// }
-		// else {tipText = sourceData;}
-
-		// var eX = this.__data__.x;
-		// var plchldrWidth = $(".l-box.asylumseekers.chart").width();
-		// var tipPos = getTopLeft(thiz.type_id, "tooltip", d3.event);
-		// var marLeft = parseInt($("#chart").css('margin-left'));
-		// var chartWidth = $("#chart").width();
-
-		// if(eX == 0 && width > 525){
-		  // eX = 0 - marLeft;
-		// }
-		// else if(eX == 0 && width <= 525){
-		  // eX = 25;
-		// }
-		// else if(eX !== 0 && width < 525){
-		  // eX = width - $(".sank_tooltip").width() - 45;
-		// }
-		// else{
-		  // var tipWidth = $(".sank_tooltip").width();//(d3.select(".sank_tooltip").style("width")).replace(/\px/g, '');
-		  // eX= chartWidth;//plchldrWidth - tipWidth - marLeft - (plchldrWidth*10/100);      
-		// }
-
-		// var heightSum = tipPos.top + $(".sank_tooltip").height();
-		// var tipTop;
-		// d3.select("#sank_" + continentStrip(node.name)).select("rect")[0][0].__data__.y < 405 ?
-		// tipTop = d3.select("#sank_" + node.name).select("rect")[0][0].__data__.y + (d3.select("#sank_" + node.name).select("rect")[0][0].__data__.dy/2) :
-		// tipTop = 400;
-
-		// d3.select(".sank_tooltip").style("top",tipTop + "px").style("left",eX+"px").style("display","block").style("background-color","rgba(255,255,255,0.96)").style("z-index","5");
-		// d3.select(".tooltip-country").text(this.__data__.name).style("color", col);
-		
-		// if(this.__data__.targetLinks.length > 0){
-		  // d3.select(".tooltip-value").html("<strong>" + formatNumber(this.__data__.value) + "</strong>" + " people have asked for asylum in " + "<strong>" + this.__data__.name + "</strong>" + ".</br></br>" + "<strong>Top countries</strong>");
-		// }
-		// else{
-		  // d3.select(".tooltip-value").html("<strong>" + formatNumber(this.__data__.value) + "</strong>" + " people have asked for asylum in other countries" + ".</br></br>" + "<strong>Top countries</strong>");
-		// }
-		// d3.select(".data-table").html(tipText).style("color", "rgba(51,51,51,.9");
-  
-	}
-	function nodeMouseout(){
-		d3.selectAll(".link").style("stroke-opacity", 0.05).style("stroke", "#000");
-		d3.select(this).style("display","none").style("z-index","-1");
-    }
-
-	function getTopLeft(id, className, event){
-		var ttid;
-		if(className){
-		  ttid = "."+ className;
-		}
-		else { ttid = "#"+ id; }
-		var offset = $("#my_dataviz").offset();
-		var tipPosition = {"top": event.pageY - offset.top, "left": 0};
-		return tipPosition;
-	}
-  // the function for moving the nodes
-  // function dragmove(d) {
-    // d3.select(this)
-      // .attr("transform", 
-            // "translate(" 
-               // + d.x + "," 
-               // + (d.y = Math.max( 
-                  // 0, Math.min(height - d.dy, d3.event.y)) 
-                 // ) + ")"); 
-    // sankey.relayout();
-    // link.attr("d", sankey.link() ); 
-  // }
 
 });
